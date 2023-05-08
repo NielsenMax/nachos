c4f92f1f4badbfa84adcef179d2d4269d4870c60
Tomas Lautaro Lopez <tomas.lautaro.lopez@gmail.com>
2023-05-08 14:50:08 -0300

    Feat(userland): add cp, rm and cat

--- code/userprog/args.cc
+++ code/userprog/args.cc
@@ -2,34 +2,33 @@
 /// All rights reserved.  See `copyright.h` for copyright notice and
 /// limitation of liability and disclaimer of warranty provisions.
 
-
 #include "transfer.hh"
 #include "machine/machine.hh"
 #include "threads/system.hh"
 
 #include <string.h>
 
-
-static const unsigned MAX_ARG_COUNT  = 32;
+static const unsigned MAX_ARG_COUNT = 32;
 static const unsigned MAX_ARG_LENGTH = 128;
 
 /// Count the number of arguments up to a null (which is not counted).
 ///
 /// Returns true if the number fit in the established limits and false if
 /// too many arguments were provided.
-static inline
-bool CountArgsToSave(int address, unsigned *count)
+static inline bool CountArgsToSave(int address, unsigned *count)
 {
     ASSERT(address != 0);
     ASSERT(count != nullptr);
 
     int val;
     unsigned c = 0;
-    do {
+    do
+    {
         machine->ReadMem(address + 4 * c, 4, &val);
         c++;
     } while (c < MAX_ARG_COUNT && val != 0);
-    if (c == MAX_ARG_COUNT && val != 0) {
+    if (c == MAX_ARG_COUNT && val != 0)
+    {
         // The maximum number of arguments was reached but the last is not
         // null.
         return false;
@@ -45,7 +44,8 @@ SaveArgs(int address)
     ASSERT(address != 0);
 
     unsigned count;
-    if (!CountArgsToSave(address, &count)) {
+    if (!CountArgsToSave(address, &count))
+    {
         return nullptr;
     }
 
@@ -54,16 +54,22 @@ SaveArgs(int address)
 
     // Allocate an array of `count` pointers.  We know that `count` will
     // always be at least 1.
-    char **args = new char * [count + 1];
+    char **args = new char *[count + 1];
 
-    for (unsigned i = 0; i < count; i++) {
-        args[i] = new char [MAX_ARG_LENGTH];
+    for (unsigned i = 0; i < count; i++)
+    {
+        args[i] = new char[MAX_ARG_LENGTH];
         int strAddr;
         // For each pointer, read the corresponding string.
         machine->ReadMem(address + i * 4, 4, &strAddr);
         ReadStringFromUser(strAddr, args[i], MAX_ARG_LENGTH);
+
+        DEBUG('e', "Reading argument %s.\n",
+              args[i]);
     }
-    args[count] = nullptr;  // Write the trailing null.
+    args[count] = nullptr; // Write the trailing null.
+
+    DEBUG('e', "Finish reading arguments\n");
 
     return args;
 }
@@ -81,25 +87,28 @@ WriteArgs(char **args)
     int argsAddress[MAX_ARG_COUNT];
     unsigned c;
     int sp = machine->ReadRegister(STACK_REG);
-    for (c = 0; c < MAX_ARG_COUNT; c++) {
-        if (args[c] == nullptr) {   // If the last was reached, terminate.
+    for (c = 0; c < MAX_ARG_COUNT; c++)
+    {
+        if (args[c] == nullptr)
+        { // If the last was reached, terminate.
             break;
         }
-        sp -= strlen(args[c]) + 1;  // Decrease SP (leave one byte for \0).
-        WriteStringToUser(args[c], sp);  // Write the string there.
-        argsAddress[c] = sp;        // Save the argument's address.
-        delete args[c];             // Free the string.
+        sp -= strlen(args[c]) + 1;      // Decrease SP (leave one byte for \0).
+        WriteStringToUser(args[c], sp); // Write the string there.
+        argsAddress[c] = sp;            // Save the argument's address.
+        delete args[c];                 // Free the string.
     }
-    delete args;  // Free the array.
+    delete args; // Free the array.
     ASSERT(c < MAX_ARG_COUNT);
 
-    sp -= sp % 4;     // Align the stack to a multiple of four.
-    sp -= c * 4 + 4;  // Make room for `argv`, including the trailing null.
+    sp -= sp % 4;    // Align the stack to a multiple of four.
+    sp -= c * 4 + 4; // Make room for `argv`, including the trailing null.
     // Write each argument's address.
-    for (unsigned i = 0; i < c; i++) {
+    for (unsigned i = 0; i < c; i++)
+    {
         machine->WriteMem(sp + 4 * i, 4, argsAddress[i]);
     }
-    machine->WriteMem(sp + 4 * c, 4, 0);  // The last is null.
+    machine->WriteMem(sp + 4 * c, 4, 0); // The last is null.
 
     machine->WriteRegister(STACK_REG, sp);
     return c;
