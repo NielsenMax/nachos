a0877ffea06ce686898b14d7654fb85070ec184e
Tomas Lautaro Lopez <tomas.lautaro.lopez@gmail.com>
2023-05-16 11:56:11 -0300

    Adding on demand loading

--- code/userprog/prog_test.cc
+++ code/userprog/prog_test.cc
@@ -33,7 +33,7 @@ void StartProcess(const char *filename)
     AddressSpace *space = new AddressSpace(executable);
     currentThread->space = space;
 
-    delete executable;
+    // delete executable;
 
     space->InitRegisters(); // Set the initial register values.
     space->RestoreState();  // Load page table register.
